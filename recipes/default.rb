caa47df739add31c4af56f99ff2ea9d5e9ddbd04
Travis A. Everett <travis.a.everett@gmail.com>
2014-04-23 21:12:18 -0500

    pre/normal version

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-node[:autopackage].each do |pkg|
+node[:autopackage][:pre].each do |pkg|
+	act = package pkg do
+		action :nothing
+	end
+	act.run_action(:install)
+end
+
+node[:autopackage][:normal].each do |pkg|
 	package pkg do
 		action :install
 	end
